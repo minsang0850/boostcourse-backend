1302ba269d662b91bffc77dd62c63aabdb7761b4
park <minsang0850@naver.com>
2021-08-04 10:41:14 +0900

    add apiController

---
+++ src/main/java/org/edwith/webbe/guestbook/controller/GuestbookApiController.java
@@ -0,0 +1,70 @@
+package org.edwith.webbe.guestbook.controller;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.edwith.webbe.guestbook.dto.Guestbook;
+import org.edwith.webbe.guestbook.service.GuestbookService;
+
+@RestController
+@RequestMapping(path="/guestbooks")		//guestbooks로 요청이 들어오면 매핑
+public class GuestbookApiController {
+	@Autowired
+	GuestbookService guestbookService;
+	
+	@GetMapping							//Get method로 요청이 오면
+	public Map<String, Object> list(@RequestParam(name="start", required=false, defaultValue="0") int start) {
+		
+		List<Guestbook> list = guestbookService.getGuestbooks(start);
+		
+		int count = guestbookService.getCount();	//전체 글 개수
+		int pageCount = count / GuestbookService.LIMIT;		//페이지 수
+		if(count % GuestbookService.LIMIT > 0)
+			pageCount++;
+		
+		List<Integer> pageStartList = new ArrayList<>();
+		for(int i = 0; i < pageCount; i++) {		//페이지마다 처음에 오는 게시글 저장 
+			pageStartList.add(i * GuestbookService.LIMIT);
+		}
+		
+		Map<String, Object> map = new HashMap<>();
+		map.put("list", list);
+		map.put("count", count);
+		map.put("pageStartList", pageStartList);
+		
+		return map;
+	}
+	
+	@PostMapping
+	public Guestbook write(@RequestBody Guestbook guestbook,
+						HttpServletRequest request) {
+		String clientIp = request.getRemoteAddr();
+		// id가 입력된 guestbook이 반환된다.
+		Guestbook resultGuestbook = guestbookService.addGuestbook(guestbook, clientIp);
+		return resultGuestbook;
+	}
+	
+	@DeleteMapping("/{id}")
+	public Map<String, String> delete(@PathVariable(name="id") Long id,
+			HttpServletRequest request) {
+		String clientIp = request.getRemoteAddr();
+		
+		int deleteCount = guestbookService.deleteGuestbook(id, clientIp);
+		return Collections.singletonMap("success", deleteCount > 0 ? "true" : "false");
+	}
+}
--- src/main/java/org/edwith/webbe/guestbook/service/GuestbookService.java
+++ src/main/java/org/edwith/webbe/guestbook/service/GuestbookService.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import org.edwith.webbe.guestbook.dto.Guestbook;
 
-public interface GuestbookService {
+public interface GuestbookService {		//서비스상에서 필요한 객체 (화면에 보여지는)
 	public static final Integer LIMIT = 5;
 	public List<Guestbook> getGuestbooks(Integer start);
 	public int deleteGuestbook(Long id, String ip);
