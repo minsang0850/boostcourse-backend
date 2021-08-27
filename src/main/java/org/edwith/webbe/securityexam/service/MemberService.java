ee44c588bd55d58bd3e04d3fbeeaeb3698453434
park <minsang0850@naver.com>
2021-08-27 14:28:32 +0900

    spring security

---
+++ src/main/java/org/edwith/webbe/securityexam/service/MemberService.java
@@ -0,0 +1,9 @@
+package org.edwith.webbe.securityexam.service;
+
+import org.edwith.webbe.securityexam.dto.Member;
+import org.edwith.webbe.securityexam.service.security.UserDbService;
+
+public interface MemberService extends UserDbService {
+	void addMember(Member member, boolean admin);
+	Member getMemberByEmail(String email);
+}
